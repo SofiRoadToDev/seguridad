72203c155f4be7590f9a02657f0602b0adf10c33
Sofi <sofi@gmail.com>
2023-07-17 15:44:53 -0300

    first commit

---
+++ src/main/java/com/sofi/sfi/SfiApplication.java
@@ -0,0 +1,13 @@
+package com.sofi.sfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sofi/sfi/controllers/MainController.java
@@ -0,0 +1,27 @@
+package com.sofi.sfi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MainController {
+
+    @GetMapping()
+    public String getPublic(){
+        return "THis is public resource";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "THis is admin resource";
+    }
+
+    @GetMapping("/user")
+    public String getUser(){
+        return "THis is user resource";
+    }
+
+
+}
